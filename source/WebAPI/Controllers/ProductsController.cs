cd67656849bbbf78dd2313518a7408cf504ed538
Klaus Barkhausen <klaus@5starstechnology.com>
2017-03-02 22:43:31 -0500

    Removed references to custom packages

--- source/WebAPI/Controllers/ProductsController.cs
+++ source/WebAPI/Controllers/ProductsController.cs
@@ -9,8 +9,6 @@ using System.Web;
 using System.Web.Http;
 using System.Web.OData;
 using System.Web.OData.Query;
-using Cmc.Core.ComponentModel;
-using Cmc.Core.Diagnostics;
 using SimpleODataApiWithEf.Data;
 using SimpleODataApiWithEf.Models;
 
@@ -19,118 +17,98 @@ namespace SimpleODataApiWithEf.Controllers
     public class ProductsController : ODataController
     {
         readonly ProductsContext _db = new ProductsContext();
-        private ILogger _logger;
 
         public ProductsController()
         {
-            _logger = ServiceLocator.Default.GetInstance<ILoggerFactory>().GetLogger(this);
         }
 
         [EnableQuery]
         //[EnableQuery(AllowedQueryOptions = AllowedQueryOptions.)]
         public IQueryable<Product> Get()
         {
-            using (new LogScope(_logger))
-            {
-                return _db.Products;
-            }
+            return _db.Products;
         }
 
         [EnableQuery]
         public SingleResult<Product> Get([FromODataUri] int key)
         {
-            using (new LogScope(_logger))
-            {
-                IQueryable<Product> result = _db.Products.Where(p => p.Id == key);
-                return SingleResult.Create(result);
-            }
+            IQueryable<Product> result = _db.Products.Where(p => p.Id == key);
+            return SingleResult.Create(result);
         }
 
         public async Task<IHttpActionResult> Post(Product product)
         {
-            using (new LogScope(_logger))
+            if (!ModelState.IsValid)
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-                _db.Products.Add(product);
-                await _db.SaveChangesAsync();
-                return Created(product);
+                return BadRequest(ModelState);
             }
+            _db.Products.Add(product);
+            await _db.SaveChangesAsync();
+            return Created(product);
         }
 
         public async Task<IHttpActionResult> Patch([FromODataUri] int key, Delta<Product> product)
         {
-            using (new LogScope(_logger))
+            if (!ModelState.IsValid)
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-                var entity = await _db.Products.FindAsync(key);
-                if (entity == null)
+                return BadRequest(ModelState);
+            }
+            var entity = await _db.Products.FindAsync(key);
+            if (entity == null)
+            {
+                return NotFound();
+            }
+            product.Patch(entity);
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
                 {
                     return NotFound();
                 }
-                product.Patch(entity);
-                try
-                {
-                    await _db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(key))
-                    {
-                        return NotFound();
-                    }
-                    throw;
-                }
-                return Updated(entity);
+                throw;
             }
+            return Updated(entity);
         }
         public async Task<IHttpActionResult> Put([FromODataUri] int key, Product update)
         {
-            using (new LogScope(_logger))
+            if (!ModelState.IsValid)
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
-                if (key != update.Id)
-                {
-                    return BadRequest();
-                }
-                _db.Entry(update).State = EntityState.Modified;
-                try
-                {
-                    await _db.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ProductExists(key))
-                    {
-                        return NotFound();
-                    }
-                    throw;
-                }
-                return Updated(update);
+                return BadRequest(ModelState);
             }
+            if (key != update.Id)
+            {
+                return BadRequest();
+            }
+            _db.Entry(update).State = EntityState.Modified;
+            try
+            {
+                await _db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(key))
+                {
+                    return NotFound();
+                }
+                throw;
+            }
+            return Updated(update);
         }
 
         public async Task<IHttpActionResult> Delete([FromODataUri] int key)
         {
-            using (new LogScope(_logger))
+            var product = await _db.Products.FindAsync(key);
+            if (product == null)
             {
-                var product = await _db.Products.FindAsync(key);
-                if (product == null)
-                {
-                    return NotFound();
-                }
-                _db.Products.Remove(product);
-                await _db.SaveChangesAsync();
-                return StatusCode(HttpStatusCode.NoContent);
+                return NotFound();
             }
+            _db.Products.Remove(product);
+            await _db.SaveChangesAsync();
+            return StatusCode(HttpStatusCode.NoContent);
         }
 
         private bool ProductExists(int key)
--- source/WebAPI/SimpleMessageHandler.cs
+++ source/WebAPI/SimpleMessageHandler.cs
@@ -6,36 +6,29 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
-using Cmc.Core.ComponentModel;
-using Cmc.Core.Diagnostics;
 
 namespace SimpleODataApiWithEf
 {
     public class SimpleMessageHandler : DelegatingHandler
     {
-        private readonly ILogger _logger;
 
         public SimpleMessageHandler()
         {
-            _logger = ServiceLocator.Default.GetInstance<ILoggerFactory>().GetLogger(this);
         }
 
         protected override Task<HttpResponseMessage> SendAsync(
         HttpRequestMessage request, CancellationToken cancellationToken)
         {
-            using (new LogScope(_logger))
+            // Create the response.
+            var response = new HttpResponseMessage(HttpStatusCode.OK)
             {
-                // Create the response.
-                var response = new HttpResponseMessage(HttpStatusCode.OK)
-                {
-                    Content = new StringContent("Hello!")
-                };
+                Content = new StringContent("Hello!")
+            };
 
-                // Note: TaskCompletionSource creates a task that does not contain a delegate.
-                var tsc = new TaskCompletionSource<HttpResponseMessage>();
-                tsc.SetResult(response);   // Also sets the task state to "RanToCompletion"
-                return tsc.Task;
-            }
+            // Note: TaskCompletionSource creates a task that does not contain a delegate.
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(response);   // Also sets the task state to "RanToCompletion"
+            return tsc.Task;
         }
     }
 }
